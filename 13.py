df9831d71e78762676a0f7981bf6b2d5d26fc136
Zhao sf <2649981104@qq.com>
2025-05-25 19:27:43 +0800

    13

--- 13.py
+++ 13.py
@@ -98,3 +98,17 @@ action_bound = env.action_space.high[0]  # 动作最大值
 agent = DDPG(state_dim, hidden_dim, action_dim, action_bound, sigma, actor_lr, critic_lr, tau, gamma, device)
 
 return_list = rl_utils.train_off_policy_agent(env, agent, num_episodes, replay_buffer, minimal_size, batch_size)
+
+episodes_list = list(range(len(return_list)))
+plt.plot(episodes_list, return_list)
+plt.xlabel('Episodes')
+plt.ylabel('Returns')
+plt.title('DDPG on {}'.format(env_name))
+plt.show()
+
+mv_return = rl_utils.moving_average(return_list, 9)
+plt.plot(episodes_list, mv_return)
+plt.xlabel('Episodes')
+plt.ylabel('Returns')
+plt.title('DDPG on {}'.format(env_name))
+plt.show()
